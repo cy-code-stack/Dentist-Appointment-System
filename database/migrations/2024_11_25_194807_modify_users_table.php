c5ed942ebc09c2af62a9f1593062fbd286570f56
cymon <cymonbuladaco971@gmail.com>
2024-11-25 19:50:19 +0800

    add nulaable entry users table

---
+++ database/migrations/2024_11_25_194807_modify_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('firstname')->nullable()->change();
+            $table->string('lastname')->nullable()->change();
+            $table->string('home_address')->nullable()->change();
+            $table->string('phone_number')->nullable()->change();
+            $table->string('status')->nullable()->change();
+            $table->string('role')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
