b8dbf1bc0df32d517927ca978289622c233f000c
cymon <cymonbuladaco971@gmail.com>
2024-12-02 19:08:39 +0800

    transfer file in the public folder

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -13,19 +13,22 @@ class PrintController extends Controller
 {
     public function downloadPrint(Request $request) {
         $appointment = Appointment::with('patient')->find($request->id);
-
+    
         $data = [
             'invoiceId' => $appointment->id,
             'patient' => $appointment->patient,
             'paymentAppointments' => $appointment->payment()->with('items')->get(),
         ];
-        $pdf = Pdf::loadView('invoice',[...$data]);
+        $pdf = Pdf::loadView('invoice', [...$data]);
         $filename = 'invoice_' . Str::random(10) . '.pdf';
-        $path = 'invoices/' . $filename;
-
-        Storage::disk('public')->put($path, $pdf->output());
-
-        // return  response()->download(storage_path($path));
-        return response()->json(['path' => Storage::url($path)]);
+        
+        $path = public_path('invoices/' . $filename);
+    
+        if (!file_exists(public_path('invoices'))) {
+            mkdir(public_path('invoices'), 0777, true);
+        }
+    
+        file_put_contents($path, $pdf->output());
+        return response()->json(['path' => url('invoices/' . $filename)]);
     }
 }
